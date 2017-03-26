be90390ace71210426b5ca549dd28ba9c91c1287
Talos Digital <santiago980241@gmail.com>
2017-03-25 20:13:39 -0500

    este es el commit inicial con todos los archivos

---
+++ js/iniciar.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	  $(".button-collapse").sideNav();
+	  $('.slider').slider({
+	  	height:600,
+	  	indicators: false
+	  });
+	  $('.carousel').carousel();
+	  
+})
+
+
