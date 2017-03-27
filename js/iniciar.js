fa5d759bc42fd6e4ac90d24195129df9530e520b
Talos Digital <santiago980241@gmail.com>
2017-03-26 19:26:21 -0500

    algunos cambios en el css menu index y galeria utilizando algunas imagenes

--- js/iniciar.js
+++ js/iniciar.js
@@ -1,4 +1,27 @@
 $(document).ready(function(){
+
+	//Este es el menu de todas las paginas
+$('header').append( '<nav class="">'+
+    '<div class="nav-wrapper ">'+
+      '<a href="#!" class="brand-logo" src="">I.E</a>'+
+      '<a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>'+
+     '<ul class="right hide-on-med-and-down">'+
+        '<li><a href="sass.html">Home</a></li>'+
+        '<li><a href="profesores.html">Profesores</a></li>'+
+        '<li><a href="collapsible.html">Eventos</a></li>'+
+        '<li><a href="galeria.html">Galeria</a></li>'+
+      '</ul>'+
+      '<ul class="side-nav" id="mobile-demo">'+
+        '<li><a href="sass.html">Home</a></li>'+
+        '<li><a href="profesores.html">Profesores</a></li>'+
+        '<li><a href="collapsible.html">Eventos</a></li>'+
+        '<li><a href="galeria.html">Galeria</a></li>'+
+      '</ul>'+
+    '</div>'+
+  '</nav>')
+//Fin del menu
+
+//Iniciar las propiedades de Materializecss
 	  $(".button-collapse").sideNav();
 	  $('.slider').slider({
 	  	height:600,
